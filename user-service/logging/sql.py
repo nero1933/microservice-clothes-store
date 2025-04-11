700896d04b8e2c8ef1662df4514bbf931095ad6f
nero1933 <neroigo.1933@gmail.com>
2025-04-11 17:22:14 +0300

    ADD: Logging

---
+++ user-service/logging/sql.py
@@ -0,0 +1,15 @@
+import logging
+import re
+
+from sqlalchemy import event, Engine
+
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.INFO)
+
+@event.listens_for(Engine, "before_cursor_execute")
+def log_sql_query(conn, cursor, statement, parameters, context, executemany):
+    formatted_statement = re.sub(
+        r"(\s)(SELECT|FROM|WHERE|AND|OR|JOIN|GROUP BY|ORDER BY|HAVING|INSERT|UPDATE|DELETE|LEFT|RIGHT|INNER|OUTER|ON|LIMIT|OFFSET|AS|IN|IS NULL|IS NOT NULL|NOT|BETWEEN|EXISTS)\s",
+        r"\1\n\2 ", statement.upper())
+    logging.info(f"SQL ---> STARTS\n\n{formatted_statement}\n\nSQL ---> STOPS")
