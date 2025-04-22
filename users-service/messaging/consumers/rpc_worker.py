a0e6bdfac9ae2e7bf4ecec7db456ca9aca3ff311
nero1933 <neroigo.1933@gmail.com>
2025-04-22 19:21:45 +0300

    UPDATE: rpc

--- users-service/messaging/consumers/rpc_worker.py
+++ users-service/messaging/consumers/rpc_worker.py
@@ -8,38 +8,54 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core import settings
 from core.db import AsyncSessionLocal
 from loggers import default_logger
+
+
+async def get_rpc_worker(
+    	queue_name: str,
+    	handler: Callable[[AbstractIncomingMessage, AsyncSession], Awaitable[bytes]],
+		db: bool,
+):
+	if db:
+		async with AsyncSessionLocal() as session:
+			await rpc_worker(queue_name, handler, session)
+	else:
+		await rpc_worker(queue_name, handler)
+
+
 async def rpc_worker(
     	queue_name: str,
-    	handler: Callable[[AbstractIncomingMessage, AsyncSession], bytes],
+    	handler: Callable[[AbstractIncomingMessage, AsyncSession], Awaitable[bytes]] |
+				 Callable[[AbstractIncomingMessage], Awaitable[bytes]],
+		db: AsyncSession | None = None,
 ) -> None:
-	async with AsyncSessionLocal() as session:
-		# Perform connection
-		connection = await connect(settings.rabbitmq_url)
+	# Perform connection
+	connection = await connect(settings.rabbitmq_url)
 
-		# Creating a channel
-		channel = await connection.channel()
-		exchange = channel.default_exchange
+	# Creating a channel
+	channel = await connection.channel()
+	exchange = channel.default_exchange
 
-		# Declaring queue
-		queue = await channel.declare_queue(queue_name, auto_delete=True)
+	# Declaring queue
+	queue = await channel.declare_queue(queue_name, auto_delete=True)
 
-		# Start listening the queue
-		async with queue.iterator() as qiterator:
-			message: AbstractIncomingMessage
-			async for message in qiterator:
-				try:
-					default_logger.info(f"[x] Received message {message.correlation_id}")
+	# Start listening the queue
+	async with queue.iterator() as qiterator:
+		message: AbstractIncomingMessage
+		async for message in qiterator:
+			try:
+				default_logger.info(f"[x] Received message {message.correlation_id}")
 
-					response = await handler(message, session)
-					await exchange.publish(
-						Message(
-							body=response,
-							correlation_id=message.correlation_id,
-						),
-						routing_key=message.reply_to,
-					)
-					default_logger.info(f"[.] Message {message.correlation_id} completed")
+				response = await handler(message, db) if db else await handler(message)
+				await exchange.publish(
+					Message(
+						body=response,
+						correlation_id=message.correlation_id,
+					),
+					routing_key=message.reply_to,
+				)
 
-				except Exception:
-					default_logger.exception(f"[!] Processing error for {message.correlation_id}")
+				default_logger.info(f"[.] Message {message.correlation_id} completed")
+
+			except Exception:
+				default_logger.exception(f"[!] Processing error for {message.correlation_id}")
 
--- users-service/worker.py
+++ users-service/worker.py
@@ -1,8 +1,18 @@
 import asyncio
 
-from messaging.consumers.rpc_worker import rpc_worker
+from loggers import default_logger
+from messaging.consumers.rpc_worker import get_rpc_worker
 from messaging.consumers.handlers import handle_auth_authenticate
 
 
+async def main():
+	workers = [
+		get_rpc_worker("rpc.users.get_auth_data", handle_auth_authenticate, db=True)
+	]
+
+	await asyncio.gather(*workers)
+
+
 if __name__ == "__main__":
-	asyncio.run(rpc_worker("rpc.users.get_auth_data", handle_auth_authenticate))
+	default_logger.info("* * Worker started")
+	asyncio.run(main())
