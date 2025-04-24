2aa2a1d34f1f09d6895a0d40a4973cdf82ea16f8
nero1933 <neroigo.1933@gmail.com>
2025-04-24 16:06:38 +0300

    UPDATE: Messaging (fix)

--- users-service/worker.py
+++ users-service/worker.py
@@ -4,7 +4,6 @@ from core import settings
 from core.messaging import BaseMessagingConnection
 from loggers import default_logger
 # from messaging.consumers.rpc_worker import get_rpc_worker
-from messaging.workers.handlers import handle_auth_authenticate
 from messaging.workers.rpc_worker import test_worker
 
 
