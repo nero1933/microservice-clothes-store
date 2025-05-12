39783579a35f355e98a8590cf32030f57e61999c
nero1933 <neroigo.1933@gmail.com>
2025-05-12 13:00:07 +0300

    UPDATE: Workers (signal handlers)

--- microservices/email/worker.py
+++ microservices/email/worker.py
@@ -1,21 +1,35 @@
 import asyncio
+import signal
 
 from config import settings
 from core.messaging import BaseMessagingConnection
 from core.loggers import log
-from workers.send_email import SendEmailWorker
+from workers.send_email import SendEmailWorker, SendResetPasswordEmailWorker
+
+shutdown_event = asyncio.Event()
+
+
+def shutdown():
+	log.info("Shutdown signal received.")
+	shutdown_event.set()
 
 
 async def main():
-	workers = (SendEmailWorker, )
+	workers = (SendResetPasswordEmailWorker,)
 	rabbit = BaseMessagingConnection()
+
 	await rabbit.setup_connection(settings.rabbitmq_url)
 	for worker in workers:
 		await worker.create_worker()
 
+	loop = asyncio.get_running_loop()
+	loop.add_signal_handler(signal.SIGINT, shutdown)
+	loop.add_signal_handler(signal.SIGTERM, shutdown)
+
 	try:
-		await asyncio.Future()
+		await shutdown_event.wait()
 	finally:
+		log.info("Shutting down gracefully...")
 		await rabbit.disconnect()
 
 
--- microservices/users/worker.py
+++ microservices/users/worker.py
@@ -1,21 +1,40 @@
 import asyncio
+import signal
 
 from config import settings
 from core.messaging import BaseMessagingConnection
 from core.loggers import log
 from workers.rpc import UsersAuthenticateRPC
 
+shutdown_event = asyncio.Event()
+
+
+def shutdown():
+	log.info("Shutdown signal received.")
+	shutdown_event.set()
+
 
 async def main():
+	# workers = (SendResetPasswordEmailWorker,)
 	RPCs = (UsersAuthenticateRPC, )
 	rabbit = BaseMessagingConnection()
+
 	await rabbit.setup_connection(settings.rabbitmq_url)
 	for RPC in RPCs:
 		await RPC.register()
 
+	# await rabbit.setup_connection(settings.rabbitmq_url)
+	# for worker in workers:
+	# 	await worker.create_worker()
+
+	loop = asyncio.get_running_loop()
+	loop.add_signal_handler(signal.SIGINT, shutdown)
+	loop.add_signal_handler(signal.SIGTERM, shutdown)
+
 	try:
-		await asyncio.Future()
+		await shutdown_event.wait()
 	finally:
+		log.info("Shutting down gracefully...")
 		await rabbit.disconnect()
 
 
